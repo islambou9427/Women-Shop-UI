a52ba9fa327809777ce0edb77b1cd394e2a8e45f
islambou9427 <Vous@exemple.com>
2020-08-08 00:43:34 +0200

    finish appbar

--- WomenShopUi/lib/screens/produitDetail/Detail_screen.dart
+++ WomenShopUi/lib/screens/produitDetail/Detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:WomenShopUi/Models/Products.dart';
+import 'package:WomenShopUi/screens/produitDetail/Body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -9,6 +11,30 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: product.color,
+      appBar: buildAppBar(context),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('WomenShopUi/assets/icons/back.svg'),
+        onPressed: () => Navigator.pop(context),
+      ),
+      actions: <Widget>[
+        IconButton(
+            icon: SvgPicture.asset('WomenShopUi/assets/icons/search.svg'),
+            onPressed: () {}),
+        IconButton(
+            icon: SvgPicture.asset('WomenShopUi/assets/icons/cart.svg'),
+            onPressed: () {}),
+        SizedBox(
+          width: 10,
+        )
+      ],
     );
   }
 }
