863d2b36ec639c2091d9bd8c68186dfe3b8a9418
islambou9427 <Vous@exemple.com>
2020-08-08 02:45:24 +0200

    body screen

--- WomenShopUi/lib/screens/produitDetail/Body.dart
+++ WomenShopUi/lib/screens/produitDetail/Body.dart
@@ -1,8 +1,21 @@
+import 'package:WomenShopUi/Models/Products.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({Key key, this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            
+          )
+        ],
+      ),
+    );
   }
 }
